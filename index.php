e624df37e00526a58ca0437430f881b78bc97f55
Ben M. Ward <ben.ward@gmail.com>
2011-08-21 12:43:45 -0600

    Added initial load sequence

--- index.php
+++ index.php
@@ -5,4 +5,36 @@
  * http://creativecommons.org/licenses/by/3.0/
  * Based on a work at https://github.com/Foran/E-Storefront.
  ***********************************************************/
+
+// Initialize the Session
+session_start();
+
+// Load Basic utility classes
+require_once('Classes/Utility/Config.php');
+require_once('Classes/Utility/Database.php');
+
+//Include the template engine
+require_once('Includes/Utilities/Templates.php');
+
+// If the installer is present, transfer control
+if(file_exists('Installer/installer.php')) require_once('Installer/installer.php');
+
+//Load the static configuration
+$global_Config = new Utility_Config('Main');
+global $global_Config;
+
+//Load and connect to the database
+$global_Database = new Utility_Database();
+global $global_Database;
+$global_Database->Connect(@$global_Config->get_Database_Host(), @$global_Config->get_Database_Username(), @$global_Config->get_Database_Password(), @$global_Config->get_Database_Database());
+$global_Database->set_Prefix($global_Config->get_Database_Prefix());
+
+//Load the Permalink
+$global_Permalink = isset($_SERVER['REDIRECT_URL']) ? $_SERVER['REDIRECT_URL'] : @$_SERVER['REQUEST_URI'];
+global $global_Permalink;
+
+//Load the visitors IP Address
+$global_IP_Address = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : (isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) ? $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'] : @$_SERVER['REMOTE_ADDR']);
+global $global_IP_Address;
+
 ?>
